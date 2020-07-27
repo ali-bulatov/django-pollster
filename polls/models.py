9a099938af7ef1598ae7295557fc722e9a508c24
Alikhan Bulatov <ali-bulatov@protonmail.com>
2020-07-27 06:14:07 -0400

    Question and choices models created

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# id is created automatically
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    # Link Foreign key to primary key
+    # CASCADE - if question is deleted all choices are deleted too 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
