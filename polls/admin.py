f6b0a2418bf099de1f46efb55a64c9ad2a896810
Alikhan Bulatov <ali-bulatov@protonmail.com>
2020-07-27 06:39:13 -0400

    Questions admin backend view created

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+# Add choice inline fields
+class ChoiceInline(admin.TabularInline):
+    model = Choice 
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
